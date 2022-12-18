aebb5e4395538900cffc42b76085a6ae222c7097
Smrat Ritwik <120279559+SmratRitwik@users.noreply.github.com>
2022-12-18 17:22:02 +0530

    Time: 470 ms (28.26%), Space: 98.6 MB (89.86%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int>nge(n, 0); // initially all 0, stores distance between their next greater element and current temperature
-        stack<int>st{};
-        
-		// move from right to left
-        for(int i = n-1; i>=0; --i){
-            // pop until we find next greater element to the right
-			// since we came from right stack will have element from right only
-			// s.top() is the index of elements so we put that index inside temperatures vector to check
-            while(!st.empty() && temperatures[st.top()] <= temperatures[i])
-                st.pop();
-				
-            // if stack not empty, then we have some next greater element, 
-			// so we take distance between next greater and current temperature
-			// as we are storing indexes in the stack
-            if(!st.empty())
-                nge[i] = st.top()-i; // distance between next greater and current
-            
-			// push the index of current temperature in the stack,
-			// same as pushing current temperature in stack
-            st.push(i);
+    vector<int> dailyTemperatures(vector<int>& T) {
+        if (T.empty())
+        {
+            return vector<int>();
         }
         
-        return nge;
+		// A stack with values:  
+		// <T[i], Number of days to the next larger number in T>
+        stack<pair<int, int>> s;
+		
+		// The last number in T must have no value larger than it.
+        s.push(make_pair(T.back(), 0));
+        
+        vector<int> ans(T.size(), 0);
+        
+		// Start from the back.
+        for (int i = T.size() - 2; i >= 0; i--)
+        {
+            int counter = 1;
+            
+			// Pop the stack until its empty or the top number is smaller or equal to T[i]
+			// The counter + the number of elements to the next larger number in T.
+            while (!s.empty() && T[i] >= s.top().first)
+            {
+                counter += s.top().second;
+                s.pop();
+            }
+            
+			// If no number on stack is larger than T[i].
+            if (s.empty())
+            {
+                s.push(make_pair(T[i], 0));
+            }
+            else
+            {
+                s.push(make_pair(T[i], counter));
+                ans[i] = counter;
+            }
+        }
+        
+        return ans;
     }
 };
