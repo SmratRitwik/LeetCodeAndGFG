a785df683aa60e64a76b7a6a2108f706ec606476
Smrat Ritwik <120279559+SmratRitwik@users.noreply.github.com>
2022-12-24 23:22:46 +0530

    Time: 4 ms (44.75%), Space: 6.5 MB (51.09%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int numTilings(int n) {
-        vector<long long>dp(n + 1);
-        dp[1] = 1; dp[0] = 1;
-        int MOD = 1000000007;
-        for (int width = 2; width <= n; width++)
-        {
-            long long totalWays = 0;
-            for (int fixWidth = 1; fixWidth <= width; fixWidth++)
-            {
-                int leftWidth = width - fixWidth;
-                long long ways = dp[leftWidth]; 
-                if (fixWidth > 2) ways = (ways * 2) % MOD; //because from fixWidth > 2, two configurations exist for each case
-                //fixWidthConfiguration appended to all dp[leftWidth] configurations
-                
-                totalWays = (totalWays + ways) % MOD;
-            }
-            dp[width] = totalWays;
-        }
-        return dp[n];
+        if(n<3)
+        return n;
+        int md = 1e9+7;
+        vector<long long int> v(n+1, 0);
+        v[1] = 1;
+        v[2] = 2;
+        v[3] = 5;
+        if(n<3)
+        return n;
+        if(n<4)
+            return v[n];
+        for(int i=4; i<=n; i++){
+            v[i] = (2 * v[i-1]) + v[i-3];  
+            v[i] %= md;
+        }      
+        return v[n];        
     }
 };
