8b2fc35d79d23105ac3bbaa2435edd65ba734d2e
Smrat Ritwik <120279559+SmratRitwik@users.noreply.github.com>
2023-01-23 23:12:45 +0530

    Time: 287 ms (33.26%), Space: 62.6 MB (59.27%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        //this condition is to check if given input is empty but n==1
-		
-		if(trust.empty() && n==1)
+    int findJudge(int k, vector<vector<int>>& trust) {
+        int n = trust.size();
+        if(k==1 && n==0)
             return 1;
-		
-		//counts outdegree of a person or node 
-		//outdegree means number of nodes that are trusted by given specific node 
-        
-		vector<int> out(n+1,0);
-		
-		//counts indegree of a person or node 
-		//indegree means number of nodes that trusts a given specific node
-        
-		vector<int> in(n+1,0);
-		
-		//loop counts indegree and outdegree
-        
-		for(auto edge : trust){
-            out[edge[0]]++;
-            in[edge[1]]++;
+        if(n<k-1 || k==1)
+            return -1;
+        set<int> st;
+        map<int,int> mp;
+        for(int i=0; i<n; i++){
+            st.insert(trust[i][0]);
+            mp[trust[i][1]]++;
         }
-		
-		//if a node has outdegree==0 that means that node doesnot trust anyone and
-		//indegree ==n-1 that means all other nodes trusts this node except himself
-        
-		for(int i=0;i<=n;i++){
-            if(out[i]==0 && in[i]==n-1)
-                return i;
+        if(st.size()!=k-1)
+            return -1;
+        for(auto x : mp){
+            if(x.second==st.size())
+                return x.first;
         }
-		//if there is no such node return -1
         return -1;
     }
 };
