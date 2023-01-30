e865fc3a099679418cd9f9dab3eb33e8f8882173
Smrat Ritwik <120279559+SmratRitwik@users.noreply.github.com>
2023-01-30 15:36:28 +0530

    Time: 3 ms (38.83%), Space: 6 MB (73.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+int arr[38] ={0};
+class Solution {
+    void fill(){
+        arr[0]=0, arr[1]=1, arr[2]=1;
+        for(int i=3; i<=37; i++){
+            arr[i] = (arr[i-1]+arr[i-2]+arr[i-3]);
+        }
+    }
+public:
+    int tribonacci(int n) {
+        fill();
+        return arr[n];
+    }
+};
