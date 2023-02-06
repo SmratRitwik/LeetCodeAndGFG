22f830ca5c9d6ef0dcb39ac6f9343dc2c153dd22
Smrat Ritwik <120279559+SmratRitwik@users.noreply.github.com>
2023-02-06 10:43:20 +0530

    Time: 11 ms (28.61%), Space: 9.8 MB (61.93%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        if(n==1)
-            return nums;
         vector<int> ans;
         for(int i=0; i<n; i++){
             ans.push_back(nums[i]);
