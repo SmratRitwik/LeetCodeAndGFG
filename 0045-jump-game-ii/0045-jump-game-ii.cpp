59ae1f761d5643250224b7b9e441c621d415ada9
Smrat Ritwik <120279559+SmratRitwik@users.noreply.github.com>
2023-02-08 19:07:07 +0530

    Time: 3 ms (99.97%), Space: 16.6 MB (56.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    int jump(vector<int>& arr) {
-        int n = arr.size();
-        if (n == 1) 
-            return 0; 
-      
-        int maxReach = arr[0];
-        int step = arr[0];
-        int jump = 1;
-      
-        for (int i = 1; i < n; i++) 
-        { 
-            if (i == n-1) 
-                // return jump; 
-                break;
-      
-            maxReach = max(maxReach, i+arr[i]); 
-
-            step--; 
-      
-            if (step == 0) 
-            { 
-                jump++; 
-      
-                // if(i >= maxReach) 
-                //     return -1; 
-      
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return 0;       
+        int maxReach = nums[0], step = nums[0], jump = 1;      
+        for (int i = 1; i < n; i++) { 
+            if (i == n-1) break;      
+            maxReach = max(maxReach, i+nums[i]); 
+            step--;       
+            if (step == 0) { 
+                jump++;      
                 step = maxReach - i; 
             } 
         } 
       return jump;
-        // return -1; 
     }
 };
