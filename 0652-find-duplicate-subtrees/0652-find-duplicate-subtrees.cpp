37d5bf18a875e38ea05fe6d64ba9ba9ef211ee25
Smrat Ritwik <120279559+SmratRitwik@users.noreply.github.com>
2023-02-28 19:51:54 +0530

    Time: 35 ms (52.05%), Space: 43.3 MB (39.63%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<TreeNode*> vec;
+    unordered_map<string,int> ump;
+    string solve(TreeNode* root){
+        if(!root) return "$";
+        string left = solve(root->left);
+        string right = solve(root->right);
+        string s = to_string(root->val) +"$"+left+"$"+right;
+        if(ump[s]==1){
+            vec.push_back(root);
+        }
+        ump[s]++;
+        return s;
+    }
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        if(!root) return vec;
+        solve(root);
+        return vec;
+    }
+};
